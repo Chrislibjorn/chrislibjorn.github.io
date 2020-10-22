3720582871e880ec81b07f9bf89de2a1e8da0b4b
Chrislibjorn <christofferholstthomsen@gmail.com>
2020-10-22 10:25:00 +0200

    opdatering

---
+++ filer/p5lib/stickman/sketch.js
@@ -0,0 +1,39 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  function draw() {
+    background(100,100,100);
+    line(200,275,200,175)
+    line(200,275,175,380)
+    line(200,275,225,380)
+    line(200,175,175,280)
+    line(200,175,225,280)
+    fill(200,200,200)
+    triangle(150,175,250,175,200,100)
+    fill(255,255,255)
+    circle(170,100,50)
+    fill(255,255,255)
+    circle(230,100,50)
+    fill(0,0,0)
+    circle(153,110,10)
+    fill(0,0,0)
+    circle(247,90,10)
+    fill(200,50,50)
+    ellipse(200,154,40,30)
+    {
+      fill(0,0,0)
+      textSize(53)
+      text('pog',50,53)
+      fill(255,255,255)
+      textSize(50)
+      text('pog',50,50)
+      {
+        
+        fill(0,0,0)
+        rect(53,67,82,5)
+        fill(255,255,255)
+        rect(52,65,82,5)
+      }
+    }
+  }
+  
