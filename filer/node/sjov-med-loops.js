91dc438e850728fc9a1f3f6f467e96c7aa582698
Chrislibjorn <christofferholstthomsen@gmail.com>
2020-11-11 20:55:22 +0100

    fordi rasmus sagde jeg skulle

--- filer/node/sjov-med-loops.js
+++ filer/node/sjov-med-loops.js
@@ -13,3 +13,26 @@ console.log("opgave1,2")
 for (let e=10; e >= 0; e--) {
     console.log(e)
 }
+{
+    console.log("opgave2")
+    for (let i=1;i<=20;i++)
+    for (let b=1; b<1000; b+i) {
+        console.log(b) 
+}
+}
+{
+    //opgave 3
+console.log("opgave3")
+for(let y=2;y<=100;y+2)
+for(let u=3;u<=100;u+3)
+for(let i=4;i<=100;i+4)
+for(let o=5;o<=100;o+5){
+    console.log(y)
+    console.log(u)
+    console.log(i)
+    console.log(o)
+}
+}
+
+
+
