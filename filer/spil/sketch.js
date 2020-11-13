123a588fbde7360b7a8dc23acfe75bb1b0a69f56
Chrislibjorn <christofferholstthomsen@gmail.com>
2020-11-13 15:43:25 +0100

    made game progression

--- filer/spil/sketch.js
+++ filer/spil/sketch.js
@@ -7,26 +7,37 @@ let p = 0
 let h = 0
 //points
 let t = 0
+//background color
+let r = 0
+let g = 50
+//speed
+let v1 = 1
+let v=v1
 function setup() {
     createCanvas(windowWidth, windowHeight);
-    xs = random(20, width-25)
+    xs = random(20, width - 25)
 }
 function draw() {
-    v=windowHeight/90
     let x = mouseX;
-    background(20, 20, 20)
-    if (mouseIsPressed){
+    background(r, g, 20)
+    if (mouseIsPressed) {
         c++
-    }else{
-        c=0
+    } else {
+        c = 0
     }
     //laser
-    if (c>0 && c<10) {
+    if (c > 0 && c < 10) {
         fill(255, 20, 20)
         rect(x - 1.5, !height, 3, height)
         if ((xs - 10 <= x) && (xs + 10 >= x)) {
             p = 1;
+            //tæller point
+            v=v+0.1;
             t++;
+            g--;
+            if (g <= 20 && r < 50) {
+                r++;
+            }
         }
     } else {
         p = 0
@@ -36,25 +47,35 @@ function draw() {
     rect(x - 10, windowHeight - 40, 20, 400)
     //hitmaker
     //enemy model
-    if ((y2 < windowHeight-10) && (p == 0)) {
-        y2=y2+v
+    if ((y2 < windowHeight - 10) && (p == 0)) {
+        y2 = y2 + v
         circle(xs, y2, 20);
     } else {
-        xs = random(20, width-25);
+        xs = random(20, width - 25);
         y2 = 0;
 
     }
-    if(t>h){
+    //highscore counter
+    if (t > h) {
         h++;
     }
-    if(y2>windowHeight-10){
-        t=0
+    //reset
+    if (y2 > windowHeight - 10) {
+        t = 0;
+        r = 0;
+        g = 50
+        v = v1
+
+    }
+    {
+        var vt = v.toFixed(1);
     }
     {
         p = 0;
     }
     fill(255)
     textSize(20)
-    text('hits: '+t, 50, 53)
-    text('highscore: '+h, 50, 80)
+    text('hits: ' + t, 50, 50)
+    text('highscore: ' + h, 50, 80)
+    text('speed'+vt,50,110)
 }
